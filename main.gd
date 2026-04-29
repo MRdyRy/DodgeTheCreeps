68bac3ae14c0f1538e54a6753bf6c64662f3c755
Rudy Ryanto <rudyryanto@MacBook-Pro.local>
2026-04-29 19:17:00 +0700

    update the HUD

--- main.gd
+++ main.gd
@@ -5,23 +5,29 @@ var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	new_game()
+	pass
 
 func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game() -> void:
+	print("new game called")
 	score = 0
 	$player._start($StartPosition.position)
 	$StartTimer.start()
-
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready!")
+	get_tree().call_group("mobs","queue_free")
+		
 func _on_start_timer_timeout() -> void:
 	$MobTimer.start()
 	$ScoreTimer.start()
 
 func _on_score_timer_timeout() -> void:
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_mob_timer_timeout() -> void:
